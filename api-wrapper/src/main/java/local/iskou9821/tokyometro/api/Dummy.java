1dc20828f23977d8923fdcf44f198b9b7ffd0cc9
iskou9821 <kou.ishikawa@gmail.com>
2014-09-26 22:11:20 +0900

    初期プロジェクトオーファイル

---
+++ api-wrapper/src/main/java/local/iskou9821/tokyometro/api/Dummy.java
@@ -0,0 +1,12 @@
+package local.iskou9821.tokyometro.api;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Dummy {
+	private static Logger log = LoggerFactory.getLogger(Dummy.class);
+	
+	public static void main(String[] args) {
+		log.info("hogehoge");
+	}
+}
---
+++ api-wrapper/src/test/java/local/iskou9821/tokyometro/api/DummyTest.java
@@ -0,0 +1,9 @@
+package local.iskou9821.tokyometro.api;
+
+import junit.framework.TestCase;
+
+public class DummyTest extends TestCase {
+	public void testTest() {
+		//ナニモシナイ
+	}
+}
