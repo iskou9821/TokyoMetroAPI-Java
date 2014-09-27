404c6f0fac7cd7912dfa1951dfa6c0c0bede188e
iskou9821 <kou.ishikawa@gmail.com>
2014-09-27 21:31:08 +0900

    RailwayFare

--- api-wrapper/src/main/java/local/iskou9821/tokyometro/api/model/RailwayFare.java
+++ api-wrapper/src/main/java/local/iskou9821/tokyometro/api/model/RailwayFare.java
@@ -1,17 +1,20 @@
 package local.iskou9821.tokyometro.api.model;
 
-import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+import java.util.Date;
+
 import org.codehaus.jackson.annotate.JsonProperty;
 import org.codehaus.jackson.annotate.JsonTypeName;
 
 @JsonTypeName("odpt:RailwayFare")
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class RailwayFare extends AbsMainModel {
 	@JsonProperty("owl:sameAs")
 	private String sameAs;
 	
 	@JsonProperty("odpt:operator")
 	private String operator;
+	
+	@JsonProperty("dc:date")
+	private Date date;
 	
 	@JsonProperty("odpt:fromStation")
 	private String fromStation;
@@ -21,7 +24,16 @@ public class RailwayFare extends AbsMainModel {
 	
 	@JsonProperty("odpt:ticketFare")
 	private Integer ticketFare;
-
+	
+	@JsonProperty("odpt:childTicketFare")
+	private Integer childTicketFare;
+	
+	@JsonProperty("odpt:icCardFare")
+	private Integer icCardFare;
+	
+	@JsonProperty("odpt:childIcCardFare")
+	private Integer childIcCardFare;
+	
 	public String getSameAs() {
 		return sameAs;
 	}
